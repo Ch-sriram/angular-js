8a3f568123ebb2a908c8b90e8f9c13c4078aafbd
Ch-sriram <sriram.ch1995@gmail.com>
2023-07-21 11:21:22 +0530

    Added `3.2 Directives & 2-way Binding`

--- 03-data-binding-directives/app.js
+++ 03-data-binding-directives/app.js
@@ -2,10 +2,11 @@
 var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', '$timeout', function ($scope, $timeout) {
-  $scope.name = "Ram"; //  Data inside `$scope` can be interpolated inside the region of HTML where this controller - `mainController` is bound.
-                       //+ In HTML, `$scope.name` can be accessed using interpolation symbols - {{ <scope-data-variable> }}.
-                       //+ Eg: { { name } } --No need to mentioned`$scope` in HTML
-  
-  $timeout(() => $scope.name = "Everybody", 3000);
+angularApp.controller('mainController', ['$scope', '$filter', function ($scope, $filter) {
+  $scope.handle = "";
+  $scope.lowercasedHandle = () => $filter('lowercase')($scope.handle);
+
+  /**
+   * `$scope.handle` is going to change dynamically as the user types in from the input
+   */
 }]);
